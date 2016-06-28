ec83c14a8195991d8c4d2695601a32051f241001
xalava <xavier.lavayssiere@gmail.com>
2016-06-28 13:10:29 +0200

    SiteConstant

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,53 @@
-console.log('\'Allo \'Allo!');
+$("body").scrollspy({
+  offset: 52, 
+  target: "#navbar-collapsible"
+});
+
+
+// smooth scrolling
+$('a[class*="navlink"]').click(function () {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+        var a = $(this.hash);
+        if (a = a.length
+            ? a
+            : $("[name=" + this.hash.slice(1) + "]"), a.length) 
+            return $("body").animate({
+                scrollTop: a
+                    .offset()
+                    .top - 30
+            }, 800),
+            "#section1" == this.hash
+                ? $(".scroll-up").hide()
+                : $(".scroll-up").show(),
+            a.find(".animate").delay(1200).addClass("animated"),
+            setTimeout(function () {
+                a
+                    .find(".animated")
+                    .removeClass("animated")
+            }, 2e3),
+            !1
+    }
+});
+
+// displaybar
+(function ($) {
+  $(document).ready(function(){
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 100) {
+                    $('.mainbar').fadeIn({queue: false});
+                                    // $('.navbar-brand').fadeOut();
+
+                    // $(".navbar-brand").animate({right: "+=400px"},1200);
+                    // $(".navbar-brand").animate({right: "-=490px"},4500,"swing");
+                } else {
+                    $('.mainbar').fadeOut();
+                }
+            });
+        });
+
+    });
+  }(jQuery)); 
