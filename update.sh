59f380215a0dd8d132fad34d64d3e32c5380491a
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-04-24 17:04:44 +0200

    blabla

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -x
+
 gitaj $@
 gulp build
 cd ../ECAN      
 rm -r *
 cp -r ../ecanGenerator/dist/* .
+mkdir smart-contracts-etude
+mkdir smart-contracts-etude/assets
+cp -r ../SCAdemy/assets/* ./etude-smart-contracts/assets/
+cp -r ../SCAdemy/index.html ./etude-smart-contracts/
+mkdir fabidee
+cp -r ../fabidee/* ./fabidee/
 gitaj $@
